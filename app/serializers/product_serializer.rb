a1aa1f04e5e61d8c669d5f7dfcc53e1b614e3fa5
Derick Quartey <niikwartey1@gmail.com>
2016-10-09 15:42:04 -0400

    Done.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,26 +1,13 @@
-# class ProductSerializer
-#   def self.serialize(product)
-#     serialized_product = '{'
- 
-#     serialized_product += '"id": ' + product.id.to_s + ', '
-#     serialized_product += '"name": "' + product.title + '", '
-#     serialized_product += '"price": "' + product.price + '", '
-#     serialized_product += '"inventory": "' + product.inventory + '", '
-#     serialized_product += '"description": "' + product.description + '" '
-
-#     serialized_product += '}'
-#   end
-# end
-
-
 class ProductSerializer
   def self.serialize(product)
-    serialized_product = {
-      "id" => product.id,
-      "name" => product.name,
-      "price" => product.price,
-      "inventory" => product.inventory,
-      "description" => product.description,
-    }
+    serialized_product = '{'
+ 
+    serialized_product += '"id": ' + product.id.to_s + ', '
+    serialized_product += '"name": "' + product.name + '", '
+    serialized_product += '"price": "' + product.price.to_s + '", '
+    serialized_product += '"inventory": "' + product.inventory.to_s + '", '
+    serialized_product += '"description": "' + product.description.to_s + '" '
+
+    serialized_product += '}'
   end
 end
